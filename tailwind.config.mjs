b40bcbdd6ee899aec1ec64167a3ceed4d6ba6fe7
cassigs <ccgall@gmail.com>
2025-12-29 14:50:27 -0800

    Update favicon, update links

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,6 +12,16 @@ export default {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
         mono: ["JetBrains Mono", ...defaultTheme.fontFamily.mono],
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            "--tw-prose-body": "#1c1917",      // stone-800
+            "--tw-prose-invert-body": "#F3F4F6",      // stone-50
+            // "--tw-prose-headings": "#111827",  // stone-900
+            // "--tw-prose-links": "#2563EB",
+          },
+        }
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
